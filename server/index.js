75818cafe6915876d7ae88041cc5524a98e7f81b
Kundan <kraj37932@gmail.com>
2026-06-13 01:26:04 +0530

    deploying

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 app.use(cors({
         origin: ["https://interviewagent-client-x1nn.onrender.com",
-            "https://interview-agent-amber.vercel.app/"],
+            "https://interview-agent-amber.vercel.app"],
         credentials: true,
     })
 );
